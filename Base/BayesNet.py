22f47134d88b6cd1d9397573ed170125def83a82
ysasaki6023 <ysasaki6023@gmail.com>
2013-08-11 15:35:07 +0900

    Add: - Factor - BayesNet

--- Base/BayesNet.py
+++ Base/BayesNet.py
@@ -1,1 +1,12 @@
-aaa
+import os,sys
+
+class BayesNet:
+###############################
+    class Factor:
+        def __init__(self):
+            pass
+
+###############################
+    def __init__(self):
+        pass
+
